b276ccff77146f79afccf9a9d0aacabf1b057ea5
trankr <trankristine99@gmail.com>
2022-09-21 21:45:06 +0100

    first commit

---
+++ main.py
@@ -0,0 +1,12 @@
+
+
+# q10
+def two_number_sum(int_list, target_sum):
+    output = []
+    for i in range(len(int_list)):
+        target_int = target_sum - int_list[i]
+        for n in range(i+1, len(int_list)):
+            if target_int == int_list[n]:
+                output.extend([int_list[i], int_list[n]])
+    return output
+
