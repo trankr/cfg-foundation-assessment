d4f11e9535c9cea87f7bb274ec7245b5fb058d39
trankr <85131032+trankr@users.noreply.github.com>
2022-09-22 20:22:44 +0100

    Add files via upload

---
+++ 8_SQL_Practical_Question.sql
@@ -0,0 +1,34 @@
+CREATE DATABASE OrdersInfo;
+
+USE OrdersInfo;
+
+CREATE TABLE ORDERS (
+    ord_no INT NOT NULL,
+    purch_amt FLOAT(50) NOT NULL,
+    ord_date DATE NOT NULL,
+    customer_id INT NOT NULL,
+    salesman_id INT NOT NULL
+);
+
+-- Now populating the table
+INSERT INTO ORDERS
+(ord_no, purch_amt, ord_date, customer_id, salesman_id)
+VALUES
+(70001, 150.5, '2012-10-05', 3005, 5002),
+(70009, 270.65, '2012-09-10', 3001, 5005),
+(70002, 65.26, '2012-10-05', 3002, 5001),
+(70004, 110.5, '2012-08-17', 3009, 5003),
+(70007, 948.5, '2012-09-10', 3005, 5002),
+(70005, 2400.6, '2012-07-27', 3007, 5001),
+(70008, 5760, '2012-09-10', 3002, 5001),
+(70010, 1983.43, '2012-10-10', 3004, 5006),
+(70003, 2480.4, '2012-10-10', 3009, 5003),
+(70012, 250.45, '2012-06-27', 3008, 5002),
+(70011, 75.29, '2012-08-17', 3003, 5007),
+(70013, 3045.6, '2012-04-25', 3002, 5001);
+
+SELECT customer_id, max(purch_amt) AS max_purch_amt
+FROM ORDERS
+GROUP BY customer_id
+HAVING customer_id BETWEEN 3002 AND 3007
+AND max(purch_amt) > 1000;
